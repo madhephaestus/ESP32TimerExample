ed4e000372bfe7b408b36debecb763cb3a6a591e
Kevin Harrington <mad.hephaestus@gmail.com>
2019-09-07 11:46:46 -0400

    attempt to make timer fault

--- ESPMutexDemo.ino
+++ ESPMutexDemo.ino
@@ -1,14 +1,46 @@
 #include "Arduino.h"
-//The setup function is called once at startup of the sketch
-void setup()
-{
+
+volatile int interruptCounter;
+volatile int totalInterruptCounter;
+
+// Pointer to the timer object
+hw_timer_t * timer = NULL;
+// Mutex object used for protecting variables
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+// Place the Interrupt handeler in IRAM section of memory
+void IRAM_ATTR onTimer() {
+	// Lock the mutex
+	//portENTER_CRITICAL_ISR(&timerMux);
+	// modify the global variable in the System process
+	interruptCounter++;
+	// release the lock
+	//portEXIT_CRITICAL_ISR(&timerMux);
+}
+
+void setup() {
+
 	Serial.begin(115200);
 	Serial.println("Start ESPMutexDemo");
+	// get a pointer to a timer to use
+	timer = timerBegin(3, // Timer 3
+			80, // Divider from the system clock to get to us
+			true); // Count up
+	timerAttachInterrupt(timer, // Timer to attach the interrupt
+			&onTimer, // Interrupt handeler passed to timer
+			true); // Rising edge of timer
+	timerAlarmWrite(timer, // The timer object to use
+			10, // count, now now in us
+			true); // Reload after finishes, run again and again
+	timerAlarmEnable(timer); // Enable timer
 
 }
 
-// The loop function is called in an endless loop
-void loop()
-{
-//Add your repeated code here
+void loop() {
+	// take the lock in the user code
+	//portENTER_CRITICAL(&timerMux);
+	// reset the latch, modify the global memory in user process
+	interruptCounter--;
+	// release the lock
+	//portEXIT_CRITICAL(&timerMux);
 }
