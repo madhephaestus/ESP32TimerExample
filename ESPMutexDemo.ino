44a704732d84895ef2141c8d4eb109b9b63ca471
Kevin Harrington <mad.hephaestus@gmail.com>
2019-09-07 10:36:43 -0400

    Adding ino file
    Adding the stub .ino

---
+++ ESPMutexDemo.ino
@@ -0,0 +1,14 @@
+#include "Arduino.h"
+//The setup function is called once at startup of the sketch
+void setup()
+{
+	Serial.begin(115200);
+	Serial.println("Start ESPMutexDemo");
+
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+//Add your repeated code here
+}
