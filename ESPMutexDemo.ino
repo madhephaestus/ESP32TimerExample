d78865c9fc8831fa559562d05e12aef254a476b7
Kevin Harrington <mad.hephaestus@gmail.com>
2019-09-08 14:16:04 -0400

    timer and main loop synchronized

--- ESPMutexDemo.ino
+++ ESPMutexDemo.ino
@@ -1,7 +1,6 @@
 #include "Arduino.h"
 
-volatile int interruptCounter;
-volatile int totalInterruptCounter;
+double interruptCounter;
 
 // Pointer to the timer object
 hw_timer_t * timer = NULL;
@@ -9,28 +8,28 @@ hw_timer_t * timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
 // Place the Interrupt handeler in IRAM section of memory
-void IRAM_ATTR onTimer() {
+void  onTimer() {
 	// Lock the mutex
 	//portENTER_CRITICAL_ISR(&timerMux);
 	// modify the global variable in the System process
-	interruptCounter++;
+	interruptCounter+=1.0;
 	// release the lock
 	//portEXIT_CRITICAL_ISR(&timerMux);
 }
 
 void setup() {
-
+	int count = 1250;
 	Serial.begin(115200);
-	Serial.println("Start ESPMutexDemo");
+	Serial.println("Start ESPMutexDemo "+String(count)+" microsecond timer interrupts");
 	// get a pointer to a timer to use
 	timer = timerBegin(3, // Timer 3
 			80, // Divider from the system clock to get to us
 			true); // Count up
 	timerAttachInterrupt(timer, // Timer to attach the interrupt
-			&onTimer, // Interrupt handeler passed to timer
+			&onTimer, // Interrupt handler passed to timer
 			true); // Rising edge of timer
 	timerAlarmWrite(timer, // The timer object to use
-			10, // count, now now in us
+			count, // count, now now in us
 			true); // Reload after finishes, run again and again
 	timerAlarmEnable(timer); // Enable timer
 
@@ -40,7 +39,8 @@ void loop() {
 	// take the lock in the user code
 	//portENTER_CRITICAL(&timerMux);
 	// reset the latch, modify the global memory in user process
-	interruptCounter--;
+	interruptCounter-=1.0;
+	Serial.println("Value = "+String(interruptCounter));
 	// release the lock
 	//portEXIT_CRITICAL(&timerMux);
 }
