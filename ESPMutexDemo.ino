0645a22b6d3a59f59b51d2b7ac3afa54585a61c4
Kevin Harrington <harrington@wpi.edu>
2019-09-13 11:24:24 -0400

    super simple p contrller

--- ESPMutexDemo.ino
+++ ESPMutexDemo.ino
@@ -1,24 +1,45 @@
 #include "Arduino.h"
-
-double interruptCounter;
+#include "ESP32Servo.h"
+#include "ESP32Encoder.h"
+/**
+ * Drive motor 1 10Khz full duty PWM pin
+ */
+#define MOTOR1_PWM 5
+/**
+ * Pin for setting the direction of the H-Bridge
+ */
+#define MOTOR1_DIR 4
+//Encoder pins
+#define MOTOR1_ENCA 19
+#define MOTOR1_ENCB 18
 
 // Pointer to the timer object
 hw_timer_t *timer = NULL;
-// Mutex object used for protecting variables
-portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
+ESP32Encoder encoder;
+ESP32PWM motor;
+
+float setpoint = 1000;
+float position =0;
+float output =  0;
+float err = 0;
+float pGain = 1.0;
+float outvalue = 0;
 // Place the Interrupt handeler in IRAM section of memory
 void  IRAM_ATTR onTimer() {
-	// Lock the mutex
-	//portENTER_CRITICAL_ISR(&timerMux);
-	// modify the global variable in the System process
-	interruptCounter+=1.0;
-	// release the lock
-	//portEXIT_CRITICAL_ISR(&timerMux);
+	position=encoder.getCount();
+	err = setpoint-position;
+	outvalue = err * pGain;
+	if (outvalue > 0) {
+		digitalWrite(MOTOR1_DIR, HIGH); // turn the LED on (HIGH is the voltage level)
+	} else {
+		digitalWrite(MOTOR1_DIR, LOW);
+	}
+	motor.write(abs(outvalue));
 }
 
 void setup() {
-	int count = 1250;
+	int count = 5000;
 	Serial.begin(115200);
 	Serial.println("Start ESPMutexDemo "+String(count)+" microsecond timer interrupts");
 	// get a pointer to a timer to use
@@ -33,14 +54,12 @@ void setup() {
 			true); // Reload after finishes, run again and again
 	timerAlarmEnable(timer); // Enable timer
 
+	encoder.attachHalfQuad(MOTOR1_ENCA, MOTOR1_ENCB);
+	motor.attachPin(MOTOR1_PWM, 20000, 8);
+	pinMode(MOTOR1_DIR, OUTPUT);
+
 }
 
 void loop() {
-	// take the lock in the user code
-	//portENTER_CRITICAL(&timerMux);
-	// reset the latch, modify the global memory in user process
-	interruptCounter-=1.0;
-	Serial.println("Value = "+String(interruptCounter));
-	// release the lock
-	//portEXIT_CRITICAL(&timerMux);
+	// do nothing
 }
