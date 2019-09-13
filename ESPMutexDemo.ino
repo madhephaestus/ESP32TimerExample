e9ccc8d605ad8256b05be8c8cdabcdd96c406c30
Kevin Harrington <harrington@wpi.edu>
2019-09-13 11:41:43 -0400

    Adding the H-bridge enale code

--- ESPMutexDemo.ino
+++ ESPMutexDemo.ino
@@ -13,20 +13,34 @@
 #define MOTOR1_ENCA 19
 #define MOTOR1_ENCB 18
 
-// Pointer to the timer object
-hw_timer_t *timer = NULL;
+/**
+ * Enable for all H-bridges
+ */
+#define H_BRIDGE_ENABLE             13
 
 ESP32Encoder encoder;
 ESP32PWM motor;
 
-float setpoint = 1000;
+float setpoint = 10000;
 float position =0;
 float output =  0;
 float err = 0;
-float pGain = 1.0;
+float pGain = 5.0;
 float outvalue = 0;
-// Place the Interrupt handeler in IRAM section of memory
-void  IRAM_ATTR onTimer() {
+
+void setup() {
+	Serial.begin(115200);
+	Serial.println("Start ESPMutexDemo ");
+	encoder.attachHalfQuad(MOTOR1_ENCA, MOTOR1_ENCB);
+	motor.attachPin(MOTOR1_PWM, 20000, 8);
+	pinMode(MOTOR1_DIR, OUTPUT);
+	pinMode(H_BRIDGE_ENABLE, OUTPUT);
+	digitalWrite(H_BRIDGE_ENABLE, 1);
+}
+
+void loop() {
+
+	delay(1);
 	position=encoder.getCount();
 	err = setpoint-position;
 	outvalue = err * pGain;
@@ -36,30 +50,5 @@ void  IRAM_ATTR onTimer() {
 		digitalWrite(MOTOR1_DIR, LOW);
 	}
 	motor.write(abs(outvalue));
-}
-
-void setup() {
-	int count = 5000;
-	Serial.begin(115200);
-	Serial.println("Start ESPMutexDemo "+String(count)+" microsecond timer interrupts");
-	// get a pointer to a timer to use
-	timer = timerBegin(3, // Timer 3
-			80, // Divider from the system clock to get to us
-			true); // Count up
-	timerAttachInterrupt(timer, // Timer to attach the interrupt
-			&onTimer, // Interrupt handler passed to timer
-			true); // Rising edge of timer
-	timerAlarmWrite(timer, // The timer object to use
-			count, // count, now now in us
-			true); // Reload after finishes, run again and again
-	timerAlarmEnable(timer); // Enable timer
-
-	encoder.attachHalfQuad(MOTOR1_ENCA, MOTOR1_ENCB);
-	motor.attachPin(MOTOR1_PWM, 20000, 8);
-	pinMode(MOTOR1_DIR, OUTPUT);
-
-}
-
-void loop() {
-	// do nothing
+	Serial.println("Motor data "+String(outvalue)+" Setpoint "+String(setpoint)+" pos "+String(position));
 }
