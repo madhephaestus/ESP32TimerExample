b35e5255dc8a5015bc270ad0f4f546b1a5e3d7be
Kevin Harrington <mad.hephaestus@gmail.com>
2019-10-02 17:52:47 -0400

    working DDS

--- ESPMutexDemo.ino
+++ ESPMutexDemo.ino
@@ -1,29 +1,91 @@
 #include "Arduino.h"
 #include "ESP32Servo.h"
 #include "analogWrite.h"
-
-double interruptCounter=0;
-double sinComp=0;
-double numSamples = 30;
-double frequencyTarget = 500;
-double increment = (2*PI)/numSamples;
+#include <soc/sens_reg.h>
+#include <soc/sens_struct.h>
+double interruptCounter50Hz = 0;
+double interruptCounter500Hz = 0;
+double sinComp = 0;
+double sinComp50Hz = 0;
+double numSamples = 20;
+double frequencyTarget = 400;
+int count = (1.0 / (frequencyTarget / 1000000.0)) / numSamples;
+double increment = (2 * PI) / numSamples;
+double increment50Hz = increment / 10.0;
 // Pointer to the timer object
 hw_timer_t *timer = NULL;
+portMUX_TYPE DRAM_ATTR timerMux = portMUX_INITIALIZER_UNLOCKED;
+TaskHandle_t complexHandlerTask;
 
-// Place the Interrupt handeler in IRAM section of memory
-void  IRAM_ATTR onTimer() {
-	interruptCounter+=increment;
-	if (interruptCounter>=2*PI){
-		interruptCounter=0;
+int IRAM_ATTR local_adc1_read(int channel) {
+	uint16_t adc_value;
+	SENS.sar_meas_start1.sar1_en_pad = (1 << channel); // only one channel is selected
+	while (SENS.sar_slave_addr1.meas_status != 0)
+		;
+	SENS.sar_meas_start1.meas1_start_sar = 0;
+	SENS.sar_meas_start1.meas1_start_sar = 1;
+	while (SENS.sar_meas_start1.meas1_done_sar == 0)
+		;
+	adc_value = SENS.sar_meas_start1.meas1_data_sar;
+	return adc_value;
+}
+
+void runDDS(){
+	interruptCounter500Hz += increment;
+	if (interruptCounter500Hz >= 2 * PI) {
+		interruptCounter500Hz = 0;
 	}
-	sinComp=(126.0* sin(interruptCounter))+128.0;
-	analogWrite( 26, (uint8_t)sinComp);
+	interruptCounter50Hz += increment50Hz;
+	if (interruptCounter50Hz >= 2 * PI) {
+		interruptCounter50Hz = 0;
+	}
+	sinComp = (126.0 / 3 * sin(interruptCounter500Hz)) + 128.0 / 3;
+	sinComp50Hz = (126.0 / 3 * sin(interruptCounter50Hz)) + 128.0 / 3;
+	analogWrite(26, (uint8_t) (sinComp50Hz + sinComp));
+}
+
+void studentCode(){
+	int adc = local_adc1_read(0)>>4;
+
+	if(digitalRead(0)!=0)
+		analogWrite(25, (uint8_t) adc);
+	else
+		analogWrite(25, sinComp50Hz);
+
+}
+
+
+void complexHandler(void *param) {
+	while (true) {
+		// Sleep until the ISR gives us something to do, or for 1 second
+		ulTaskNotifyTake(pdFALSE, pdMS_TO_TICKS(1000));
+		runDDS();
+		studentCode();
+	}
+}
+// Place the Interrupt handeler in IRAM section of memory
+void IRAM_ATTR onTimer() {
+	// A mutex protects the handler from reentry (which shouldn't happen, but just in case)
+	portENTER_CRITICAL_ISR(&timerMux);
+
+	// Notify complexHandlerTask that the buffer is full.
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+	vTaskNotifyGiveFromISR(complexHandlerTask, &xHigherPriorityTaskWoken);
+	if (xHigherPriorityTaskWoken) {
+		portYIELD_FROM_ISR();
+	}
+
+	portEXIT_CRITICAL_ISR(&timerMux);
+
 }
 
 void setup() {
-	int count = (1.0/(frequencyTarget/1000000.0))/numSamples;
+	xTaskCreate(complexHandler, "Handler Task", 8192, NULL, 1,
+			&complexHandlerTask);
 	Serial.begin(115200);
-	Serial.println("Start ESPMutexDemo "+String(count)+" microsecond timer interrupts");
+	Serial.println(
+			"Start ESPMutexDemo " + String(count)
+					+ " microsecond timer interrupts");
 	// get a pointer to a timer to use
 	timer = timerBegin(3, // Timer 3
 			80, // Divider from the system clock to get to us
@@ -35,8 +97,9 @@ void setup() {
 			count, // count, now now in us
 			true); // Reload after finishes, run again and again
 	timerAlarmEnable(timer); // Enable timer
+	analogRead(36);
 }
 
 void loop() {
-	Serial.println("Value = "+String(sinComp));
+
 }
